1f3f5cee774d173e961b918c3951fb035f312657
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-06-29 11:14:42 +0100

    WIP- Testing Framework

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/spaces/new', function(req, res, next) {
-  res.send('hi');
+  res.render('new');
 });
 
-router.post('/spaces/new', function(req, res, next) {
-  res.send('hi');
-  redirect('/spaces')
-});
+// router.post('/spaces/new', function(req, res, next) {
+//   res.send('hi');
+//   redirect('/spaces')
+// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- app/spec/listSpaceSpec.js
+++ app/spec/listSpaceSpec.js
@@ -1,27 +1,30 @@
 var expect = require('chai').expect;
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var chaiHttp = require('chai-http');
+var chaiDom = require('chai-dom');
 var app  = require('../app');
 
 chai.use(chaiHttp);
+chai.use(chaiDom);
 
 describe ('List a new space', function() {
 
-  it('Page exists', function(done) {
+  it('Page has correct title', function(done) {
     chai.request(app).get('/spaces/new').end(function(err, res) {
-      expect(document.querySelector('h1')).not.to.be.empty
+      // expect(document.querySelector('h1')).not.to.be.empty
+      console.log(res);
+      expect(res.text.querySelector('#title')).to.contain.text('List');
       done();
-    })
+    });
   });
 
 //  it('Navigate to spaces/new, fill in details, submit redirects to spaces', function(done) {
 //    chai.request(app).get('/spaces/new').end(function() {
 //      chai.request(app).post('/spaces/new').send({ name: 'testname'  })
-//      .end(function(err, res) {  
+//      .end(function(err, res) {
 //        expect(res).to.have.status(200);
 //        done();
 //      })
 //    })
 //  });
-}); 
+});
