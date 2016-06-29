2af9e8dc0f22b6ab48619579745e287f35c56ac2
Andy <andy.bell.github@gmail.com>
2016-06-29 14:22:51 +0100

    change test functionality to no longer need external instance of node running

--- app/test/SignUpSpec.js
+++ app/test/SignUpSpec.js
@@ -1,31 +1,41 @@
+process.env.NODE_ENV = 'test';
 const Browser = require('zombie');
-
-Browser.localhost('localhost', 3000);
+const app = require('../app');
+const http = require('http');
 
 describe("Sign Up Functionality", function(){
-  const browser = new Browser();
 
-  before(function(done){
-    browser.visit('/users/new', done);
+  before(function(){
+    this.server = http.createServer(app).listen(3000);
+    this.browser= new Browser({site: 'http://localhost:3000'});
   });
+  
+  describe("checks the page",function(){
+    before(function(done){
+      this.browser.visit('/users/new', done);
+    });
+    it("expects welcome message", function(){
+      this.browser.assert.text('#form-head', 'Please enter your details');
+    });
 
-  it("expects welcome message", function(){
-    browser.assert.text('#form-head', 'Please enter your details');
-  });
-
-  it("expect a form", function(){
-    browser.assert.attribute('form', 'method', 'post');
-    browser.assert.attribute('form', 'action', '/users/new');
-    browser.assert.element('#username');
-    browser.assert.element('#full-name');
-    browser.assert.element('#email');
-    browser.assert.element('button');
-    browser.assert.attribute('button', 'value', 'submit');
+    it("expect a form", function(){
+      this.browser.assert.attribute('form', 'method', 'post');
+      this.browser.assert.attribute('form', 'action', '/users/new');
+      this.browser.assert.element('#username');
+      this.browser.assert.element('#full-name');
+      this.browser.assert.element('#email');
+      this.browser.assert.element('button');
+      this.browser.assert.attribute('button', 'value', 'submit');
+    });
   });
 
   describe("expect a successful submission", function(){
     before(function(done){
-      browser
+      this.browser.visit('/users/new', done);
+    });
+
+    before(function(done){
+      this.browser
         .fill('email', 'test@test.com')
         .fill('full-name', 'Test')
         .fill('username', 'tester')
@@ -33,7 +43,7 @@ describe("Sign Up Functionality", function(){
     });
 
     it('is successful', function(){
-      browser.assert.success();
+      this.browser.assert.success();
     });
 
     it('presents welcome page', function(){
@@ -44,5 +54,7 @@ describe("Sign Up Functionality", function(){
   });
 
 
-
+  after(function(done){
+    this.server.close(done);
+  });
 });
