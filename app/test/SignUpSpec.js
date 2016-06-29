e569f4d3b632ced3e48fb707dd3b63174f2bc205
Carlos <chdezmar@gmail.com>
2016-06-29 11:42:15 +0100

    Mocha, Chai, PhantomJS and CasperJS testing

--- app/test/SignUpSpec.js
+++ app/test/SignUpSpec.js
@@ -1,36 +1,18 @@
-var expect = require('chai').expect;
-var request = require('request');
-var chai = require('chai');
-var chaiHTTP = require('chai-http');
-var app = require('../app');
-var chaiDOM = require('chai-dom');
-
-chai.use(chaiHTTP);
-chai.use(chaiDOM);
 
 
 describe("Sign Up Functionality", function(){
 
-  describe("Page Exists", function(){
-    it("returns a successful status code", function(done){
-      chai.request(app).get('/users/new').end(function(err, res){
-        expect(res).to.have.status(200);
-        done();
-      });
-    });
-    it("expects welcome message", function(done){
-      chai.request(app).get('/users/new').end(function(err, res, body){
-        console.log("===========================")
-        console.log(err);
-        console.log("===========================")
-        console.log(res);
-        console.log("===========================")
-        console.log(body);
-        console.log("===========================")
-        var document = res;
-        expect(document).to.have.html('Please enter your details below');
-        done();
-      });
+  before(function(){
+    casper.start('http://localhost:8000');
+  });
+
+  it("expects welcome message", function(){
+    casper.waitForSelector('#form-head', function(){
+      casper.thenOpen('http://localhost:8000/users/new');
+      '#form-head'.should.be.inDOM;
     });
   });
+
+
+
 });
