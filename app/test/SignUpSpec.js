e3de4625def0783dffddde4f3cbac103dba2e5f2
Andy <andy.bell.github@gmail.com>
2016-06-29 12:21:58 +0100

    add zombie js functionality to allow testing of interface

--- app/test/SignUpSpec.js
+++ app/test/SignUpSpec.js
@@ -1,18 +1,25 @@
+const Browser = require('zombie');
 
+Browser.localhost('localhost', 3000);
 
 describe("Sign Up Functionality", function(){
+  const browser = new Browser();
 
-  before(function(){
-    casper.start('http://localhost:8000');
+  before(function(done){
+    browser.visit('/users/new', done);
   });
 
   it("expects welcome message", function(){
-    casper.waitForSelector('#form-head', function(){
-      casper.thenOpen('http://localhost:8000/users/new');
-      '#form-head'.should.be.inDOM;
-    });
+    browser.assert.text('#form-head', 'Please enter your details');
   });
 
-
-
+  it("expect a form", function(){
+    browser.assert.attribute('form', 'method', 'post');
+    browser.assert.attribute('form', 'action', '/users/new');
+    browser.assert.element('#username');
+    browser.assert.element('#full-name');
+    browser.assert.element('#email');
+    browser.assert.element('button');
+    browser.assert.attribute('button', 'value', 'submit');
+  });
 });
